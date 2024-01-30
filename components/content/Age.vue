6d827723c3f38db11db67845a2c1d19307c996b5
mclrc <moritzamando@proton.me>
2024-01-30 13:26:56 +0100

    Dynamic age

---
+++ components/content/Age.vue
@@ -0,0 +1,8 @@
+<template>
+  <span>{{ age }}</span>
+</template>
+
+<script setup lang="ts">
+const birthday = new Date("2004-01-15");
+const age = Math.floor((new Date().getTime() - birthday.getTime()) / 31536000000);
+</script>
